1a91f4bbdb1c95443c109127f07250f475fbd3c7
Sam Stout <samuelwstout@gmail.com>
2024-09-16 08:19:08 -0500

    use tailwindUI header compoent for header/footer

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,9 +1,32 @@
 export default function Footer() {
   return (
-    <div className="border-t-2 border-t-neutral-300 mx-3 py-8 flex flex-row justify-end items-center">
-      <a href="#" className="p-2">
-        <span className="text-sm">Admin panel</span>
-      </a>
+    <div className="bg-white">
+      <header className="relative">
+        <nav aria-label="Bottom">
+          <div className="bg-white">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+              <div className="border-t border-gray-200">
+                <div className="flex h-16 items-center justify-between">
+                  <div className="flex flex-1 items-center justify-end">
+                    <div className="flex items-center lg:ml-8">
+                      <div className="ml-4 flow-root lg:ml-8">
+                        <a
+                          href="#"
+                          className="group -m-2 flex items-center p-2"
+                        >
+                          <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+                            Admin panel
+                          </span>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </header>
     </div>
   );
 }
--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,17 +1,53 @@
+"use client";
+
 import { ShoppingBagIcon } from "@heroicons/react/24/outline";
 
 export default function Header() {
   return (
-    <div className="sticky top-0 border-b-2 border-b-neutral-300 mx-3 py-8 flex flex-row justify-center items-center bg-white z-10">
-      <span>COZY THREADS</span>
-      <div className="absolute right-4 p-5">
-        <a href="#" className="flex items-center p-2">
-          <ShoppingBagIcon className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
-          <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-            0
-          </span>
-        </a>
-      </div>
+    <div className="bg-white">
+      <header className="relative">
+        <nav aria-label="Top">
+          <div className="bg-white">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+              <div className="border-b border-gray-200">
+                <div className="flex h-16 items-center justify-between">
+                  <div className="hidden lg:flex lg:flex-1 lg:items-center">
+                    <a href="#">
+                      <span>COZY THREADS</span>
+                    </a>
+                  </div>
+
+                  <a href="#" className="lg:hidden">
+                    <span>COZY THREADS</span>
+                  </a>
+
+                  <div className="flex flex-1 items-center justify-end">
+                    <div className="flex items-center lg:ml-8">
+                      <div className="ml-4 flow-root lg:ml-8">
+                        <a
+                          href="#"
+                          className="group -m-2 flex items-center p-2"
+                        >
+                          <ShoppingBagIcon
+                            aria-hidden="true"
+                            className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+                          />
+                          <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+                            0
+                          </span>
+                          <span className="sr-only">
+                            items in cart, view bag
+                          </span>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </header>
     </div>
   );
 }
