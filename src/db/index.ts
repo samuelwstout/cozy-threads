16792b8450a8600e6e2e7ff70b92628d782b35e3
Sam Stout <samuelwstout@gmail.com>
2024-09-16 16:34:26 -0500

    update libsql client package

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,6 @@
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 
-console.log("TURSO_CONNECTION_URL:", process.env.TURSO_CONNECTION_URL);
-console.log("TURSO_AUTH_TOKEN:", process.env.TURSO_AUTH_TOKEN);
-
 const client = createClient({
   url: process.env.TURSO_CONNECTION_URL!,
   authToken: process.env.TURSO_AUTH_TOKEN!,
