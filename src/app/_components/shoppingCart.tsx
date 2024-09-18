4a3606eed07ff315d89d654f67ad7ae47cb7ddae
Sam Stout <samuelwstout@gmail.com>
2024-09-18 12:54:05 -0500

    handle empty cart scenario

--- src/app/_components/shoppingCart.tsx
+++ src/app/_components/shoppingCart.tsx
@@ -1,12 +1,6 @@
 "use client";
 
-import {
-  Dialog,
-  DialogBackdrop,
-  DialogPanel,
-  DialogTitle,
-} from "@headlessui/react";
-import { XMarkIcon } from "@heroicons/react/24/outline";
+import { Dialog, DialogBackdrop, DialogPanel } from "@headlessui/react";
 import {
   useOpenShoppingCart,
   useShoppingCartProducts,
@@ -62,6 +56,8 @@ export default function ShoppingCart() {
     .map((id) => shoppingCartProducts.find((p) => p.id.toString() === id))
     .filter((product): product is Product => product !== undefined);
 
+  const isCartEmpty = uniqueProducts.length === 0;
+
   return (
     <Dialog
       open={openShoppingCart}
@@ -82,100 +78,105 @@ export default function ShoppingCart() {
             >
               <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
                 <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
-                  <div className="flex items-start justify-between">
-                    <DialogTitle className="text-lg font-medium text-gray-900">
-                      Shopping cart
-                    </DialogTitle>
-                    <div className="ml-3 flex h-7 items-center">
+                  {isCartEmpty ? (
+                    <div className="flex flex-col items-center justify-center h-full">
+                      <p className="text-xl font-medium text-gray-900 mb-4">
+                        Your cart is empty
+                      </p>
                       <button
-                        type="button"
                         onClick={setOpenShoppingCart}
-                        className="relative -m-2 p-2 text-gray-400 hover:text-gray-500"
+                        className="text-blue-600 hover:text-blue-700"
                       >
-                        <span className="absolute -inset-0.5" />
-                        <span className="sr-only">Close panel</span>
-                        <XMarkIcon aria-hidden="true" className="h-6 w-6" />
+                        Start Shopping
                       </button>
                     </div>
-                  </div>
+                  ) : (
+                    <div className="mt-8">
+                      <div className="flow-root">
+                        <ul
+                          role="list"
+                          className="-my-6 divide-y divide-gray-200"
+                        >
+                          {uniqueProducts.map((product) => (
+                            <li key={product.id} className="flex py-6">
+                              <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                                <img
+                                  alt={product.title}
+                                  src={product.imageUrl}
+                                  className="h-full w-full object-cover object-center"
+                                />
+                              </div>
 
-                  <div className="mt-8">
-                    <div className="flow-root">
-                      <ul
-                        role="list"
-                        className="-my-6 divide-y divide-gray-200"
-                      >
-                        {uniqueProducts.map((product) => (
-                          <li key={product.id} className="flex py-6">
-                            <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                              <img
-                                alt={product.title}
-                                src={product.imageUrl}
-                                className="h-full w-full object-cover object-center"
-                              />
-                            </div>
+                              <div className="ml-4 flex flex-1 flex-col">
+                                <div>
+                                  <div className="flex justify-between text-base font-medium text-gray-900">
+                                    <h3>{product.title}</h3>
+                                    <p className="ml-4">${product.price}</p>
+                                  </div>
+                                </div>
+                                <div className="flex flex-1 items-end justify-between text-sm">
+                                  <p className="text-gray-500">
+                                    Qty{" "}
+                                    {productQuantities[product.id.toString()]}
+                                  </p>
 
-                            <div className="ml-4 flex flex-1 flex-col">
-                              <div>
-                                <div className="flex justify-between text-base font-medium text-gray-900">
-                                  <h3>{product.title}</h3>
-                                  <p className="ml-4">${product.price}</p>
+                                  <div className="flex">
+                                    <button
+                                      type="button"
+                                      onClick={() =>
+                                        removeOneProduct(product.id)
+                                      }
+                                      className="font-medium text-blue-600 hover:text-blue-500"
+                                    >
+                                      Remove
+                                    </button>
+                                  </div>
                                 </div>
                               </div>
-                              <div className="flex flex-1 items-end justify-between text-sm">
-                                <p className="text-gray-500">
-                                  Qty {productQuantities[product.id.toString()]}
-                                </p>
-
-                                <div className="flex">
-                                  <button
-                                    type="button"
-                                    onClick={() => removeOneProduct(product.id)}
-                                    className="font-medium text-blue-600 hover:text-blue-500"
-                                  >
-                                    Remove
-                                  </button>
-                                </div>
-                              </div>
-                            </div>
-                          </li>
-                        ))}
-                      </ul>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
                     </div>
-                  </div>
+                  )}
                 </div>
 
-                <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
-                  <div className="flex justify-between text-base font-medium text-gray-900">
-                    <p>Subtotal</p>
-                    <p>${subtotal.toFixed(2)}</p>
-                  </div>
-                  <p className="mt-0.5 text-sm text-gray-500">
-                    Shipping and taxes calculated at checkout.
-                  </p>
-                  <div className="mt-6">
-                    <Link
-                      onClick={setOpenShoppingCart}
-                      href="/checkout"
-                      className="flex items-center justify-center rounded-md border border-transparent bg-blue-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-blue-700"
-                    >
-                      Checkout
-                    </Link>
-                  </div>
-                  <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
-                    <p>
-                      or{" "}
-                      <button
-                        type="button"
-                        onClick={setOpenShoppingCart}
-                        className="font-medium text-blue-600 hover:text-blue-500"
-                      >
-                        Continue Shopping
-                        <span aria-hidden="true"> &rarr;</span>
-                      </button>
+                {!isCartEmpty && (
+                  <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+                    <div className="flex justify-between text-base font-medium text-gray-900">
+                      <p>Subtotal</p>
+                      <p>${subtotal.toFixed(2)}</p>
+                    </div>
+                    <p className="mt-0.5 text-sm text-gray-500">
+                      Shipping and taxes calculated at checkout.
                     </p>
+                    <div className="mt-6">
+                      <Link
+                        onClick={setOpenShoppingCart}
+                        href="/checkout"
+                        className={`flex items-center justify-center rounded-md border border-transparent bg-blue-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-blue-700 ${
+                          isCartEmpty ? "opacity-50 cursor-not-allowed" : ""
+                        }`}
+                        aria-disabled={isCartEmpty}
+                      >
+                        Checkout
+                      </Link>
+                    </div>
+                    <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+                      <p>
+                        or{" "}
+                        <button
+                          type="button"
+                          onClick={setOpenShoppingCart}
+                          className="font-medium text-blue-600 hover:text-blue-500"
+                        >
+                          Continue Shopping
+                          <span aria-hidden="true"> &rarr;</span>
+                        </button>
+                      </p>
+                    </div>
                   </div>
-                </div>
+                )}
               </div>
             </DialogPanel>
           </div>
